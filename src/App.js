82928c696836889784cfd0b743f7f667bd7036e9
Aaron Whitehead <whitehead.aaron@live.com>
2019-03-22 12:29:43 -0700

    making good progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { API_BASE_URL } from "./config";
 import axios from "axios";
 import "./App.css";
 
@@ -12,36 +13,70 @@ class App extends Component {
       length: "Any Quote",
       shortQuotes: [],
       mediumQuotes: [],
-      longQuotes: []
+      longQuotes: [],
+      quoteStats: {
+        rating: [],
+        ip: []
+      },
+      buttonDisabled: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
+  getQuoteStats(input) {
+    axios.get(`${API_BASE_URL}/quotes`).then(res => {
+      let newArr = res.data.filter(item => item.quote === input);
+      if (newArr.length > 0) {
+        this.setState({
+          quoteStats: { rating: newArr[0].rating, ip: newArr[0].ip }
+        });
+      }
+      if (newArr.length === 0) {
+        this.setState({ quoteStats: { rating: [], ip: [] } });
+        axios.post(`${API_BASE_URL}/quotes`, { quote: input });
+      }
+    });
+  }
 
   getQuote() {
     if (this.state.length === "Any Quote") {
+      let num = Math.floor(Math.random() * this.state.quotes.length);
       this.setState({
-        chosenQuote: this.state.quotes[Math.floor(Math.random() * 57)]
+        chosenQuote: this.state.quotes[num]
       });
+      this.getQuoteStats(this.state.quotes[num]);
     }
     if (this.state.length === "Short Quote") {
+      let num = Math.floor(Math.random() * this.state.shortQuotes.length);
       this.setState({
-        chosenQuote: this.state.shortQuotes[Math.floor(Math.random() * this.state.shortQuotes.length)]
+        chosenQuote: this.state.shortQuotes[num]
+      }).then(() => {
+        this.getQuoteStats(num);
       });
     }
     if (this.state.length === "Medium Quote") {
+      let num = Math.floor(Math.random() * this.state.mediumQuotes.length);
       this.setState({
-        chosenQuote: this.state.mediumQuotes[Math.floor(Math.random() * this.state.mediumQuotes.length)]
+        chosenQuote: this.state.mediumQuotes[num]
+      }).then(() => {
+        this.getQuoteStats(num);
       });
     }
     if (this.state.length === "Long Quote") {
+      let num = Math.floor(Math.random() * this.state.longQuotes.length);
       this.setState({
-        chosenQuote: this.state.longQuotes[Math.floor(Math.random() * this.state.longQuotes.length)]
+        chosenQuote: this.state.longQuotes[num]
+      }).then(() => {
+        this.getQuoteStats(num);
       });
     }
   }
 
   handleClick() {
+
+    this.setState({ buttonDisabled: true });
+    setTimeout(() => this.setState({ buttonDisabled: false }), 2000);
+
     if (this.state.quotes.length === 0) {
       axios
         .get("https://ron-swanson-quotes.herokuapp.com/v2/quotes/58")
@@ -64,18 +99,39 @@ class App extends Component {
           }
           this.setState({ quotes: res.data });
           this.setState({ button: "Get a New Quote" });
+          return res;
         })
         .then(() => {
           this.getQuote();
-        })
-    }
-    else {
+        });
+    } else {
       this.getQuote();
     }
   }
 
   handleLength(event) {
     this.setState({ length: event.target.value });
+  }
+
+  getRating() {
+    let total = 0;
+    for (let i = 0; i < this.state.quoteStats.rating.length; i++) {
+      total += this.state.quoteStats.rating[i];
+    }
+
+    total = total / this.state.quoteStats.rating.length;
+
+    if (total > 0) {
+      return (
+        <p>
+          This quote has a rating of {total} out of{" "}
+          {this.state.quoteStats.rating.length} votes.
+        </p>
+      );
+    }
+    if (this.state.chosenQuote !== "") {
+      return <p>This quote doesn't have a rating yet.</p>;
+    }
   }
 
   render() {
@@ -92,6 +148,7 @@ class App extends Component {
             </select>
             <button
               className="getQuoteButton"
+              disabled={this.state.buttonDisabled}
               onClick={() => {
                 this.handleClick();
               }}
@@ -100,6 +157,7 @@ class App extends Component {
             </button>
           </div>
           <p>{this.state.chosenQuote}</p>
+          {this.getRating()}
         </header>
       </div>
     );
